499b3e1695d8100fdb1b6e21c4aaac6dcbd9930b
Johnny Phompadith <johnny.phompadith@gmail.com>
2020-04-29 16:18:50 -0700

    bat-belt update

--- Johnny-Phompadith/week-1/bat-belt/bat-belt.js
+++ Johnny-Phompadith/week-1/bat-belt/bat-belt.js
@@ -1,11 +1,40 @@
+let xs = [1, 2, 3, 4, 5, 6]
+
+// each function
 function each(xs, fn) {
   for (let i = 0; i < xs.length; i++) {
     fn(xs[i], i, xs)
   }
 };
-each([1, 2, 3], console.log)
+each(xs, console.log)
 
+// map function
 function map(xs, fn) {
-  const mapxs = []
-  for ()
+  const newinput = [];
+  for (let i = 0; i < xs.length; i++) {
+    fn(newinput[i] = xs[i] + 2, i)
+  }
+};
+map(xs, console.log)
+
+// filter
+function isOdd(x) {
+  return x % 2 === 1
 }
+
+function filter(xs, predicate) {
+  const filtered = []
+  each(xs, function (x) {
+    if (predicate(x)) {
+        filtered.push(x)
+      }
+  })
+  return filtered
+}
+console.log(filter(xs, isOdd))
+
+// random
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min+1));
+};
+console.log(random(1, 100))
