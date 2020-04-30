62d68f5864760445c8456ed3b1e7457544b7783c
Dadams4786 <58703714+Dadams4786@users.noreply.github.com>
2020-04-29 19:39:08 -0700

    moved branch because git is stupid

---
+++ Daniel-Adams/DanielAdamsClosure/src/function.js
@@ -0,0 +1,16 @@
+function add(num1, num2){
+    return num1 + num2
+};
+function sub(num1, num2){
+    return num1 - num2
+};
+function mul(num1, num2){
+    return num1 * num2
+};
+function identity(arg1){
+    return arg1
+};
+function identityf(arg1){
+    funtion()
+    return arg1
+};
