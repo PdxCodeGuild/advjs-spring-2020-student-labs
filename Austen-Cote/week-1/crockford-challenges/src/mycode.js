1e3f4b75b8ed3920a6d504b1870461d88d2f7062
austen cote <coteausten@gmail.com>
2020-04-29 18:58:40 -0700

    initial challenge commit

---
+++ Austen-Cote/week-1/crockford-challenges/src/mycode.js
@@ -0,0 +1,17 @@
+function add(x,y){
+    return x+y
+}
+
+function sub(x,y){
+    return x - y
+}
+
+function mul(x,y){
+    return x * y
+}
+
+function identity(x){
+    return x
+}
+
+function identifyf()
