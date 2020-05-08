a6a004a208fdafd855fa393a6c900f7517a4a5ae
the crafty coder <lisathuanguyen@gmail.com>
2020-05-07 20:26:26 -0700

    can read csv

--- Lisa-Nguyen/week-2/chatroom/main.js
+++ Lisa-Nguyen/week-2/chatroom/main.js
@@ -1,9 +1,26 @@
 
-// POST to /newMessage
-// GET to /messages
+/* URLs
+POST to /newMessage
+GET to /messages */
 
 /* Metadata
 - timestamp
 - message
 - sender
 - receiver */
+
+// Modules
+const csv = require('csv-parser');
+const fs = require('fs');
+
+// Variables
+const file = 'messages.csv'
+
+fs.createReadStream(file)
+  .pipe(csv())
+  .on('data', function (row) {
+    console.log(row);
+  })
+  .on('end', function(){
+    console.log('Message history complete.')
+  })
