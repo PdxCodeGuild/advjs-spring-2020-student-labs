b4db4cea52d124696e2d67ec23055f2cbda273f2
the crafty coder <lisathuanguyen@gmail.com>
2020-05-08 21:26:06 -0700

    saving

--- Lisa-Nguyen/week-2/chatroom/main.js
+++ Lisa-Nguyen/week-2/chatroom/main.js
@@ -1,72 +1,80 @@
-
-/* URLs
-POST to /newMessage
-GET to /messages */
-
-/* Metadata
-- timestamp
-- message
-- sender
-- receiver */
-
 // Modules
-const fs = require('fs');
-const http = require('http');
-const url = require('url')
-
-// files to include
-const funFaker = require('funFaker')
+const fs = require('fs')
+const http = require('http')
 
 // Variables
 const file = 'messages.csv'
-let data = '';
+let data = ''
+const port = 8000
+
+// fake data
+const characters = ['Harry', 'Ron', 'Hermione', 'Draco', 'Ginny', 'Neville', 'Luna']
+
+function fakeData () {
+  const sender = characters[Math.floor(Math.random() * characters.length)]
+  const receiver = characters[Math.floor(Math.random() * characters.length)]
+
+  return [sender, receiver]
+}
 
 // functions
 function formatData (sender, receiver, message) {
   const timestamp = Date.now()
+  while (sender === receiver) {
+    receiver = characters[Math.floor(Math.random() * characters.length)]
+  }
 
   data = ('\n' + timestamp) + ',' + sender + ',' + receiver + ',' + message
 
   return data
 }
 
-//
-function messageHistory (file) {
-  console.log("\nMessage history...\n")
-  fs.readFile(file, 'utf8', function (err, data) {
-    if (err) throw err
-    console.log(data);
-  })
+function sendMessage (data, callback) {
+  console.log('\nSending message...')
+
+  fs.appendFile(file, data, 'utf8',
+    function (err) {
+      if (err) {
+        callback(err)
+      } else {
+        callback(null)
+      }
+    })
 }
-
-function sendMessage(data){
-  console.log("\nSending message...")
-  fs.appendFile(file, data, 'utf8', function(err) {
-    if (err) throw err;
-    //console.log('Message saved.')
-  })
-}
-
-// sendMessage(data)
-// messageHistory(file)
-
-// http.createServer(function (req, res) {
-//   const urlData = url.parse(req.url, true)
-
-//   switch (urlData.pathname) {
-//     case '/newMessage':
-//       sendMessage(data)
-//       break
-//     case '/messages':
-//       messageHistory(file)
-//       break
-//     default:
-//       res.end('unsupported path')
-//   }
-// }).listen(8000)
-
 
 http.createServer(function (req, res) {
-  res.write('Hello World!');
-  res.end();
-}).listen(8080);
+  if (req.method === 'GET') {
+    const stream = fs.createReadStream(file)
+    stream.pipe(res)
+  } else if (req.method === 'POST') {
+    let text = ''
+
+    req.on('data', function (chunk) {
+      text += chunk
+      console.log(`Text: ${text}`)
+      return text
+    })
+
+    req.on('end', function () {
+      const sender = fakeData()[0]
+      const receiver = fakeData()[1]
+
+      const data = formatData(sender, receiver, text)
+
+      sendMessage(data, function (err) {
+        if (err) {
+          res.writeHead(500, { 'Content-Type': 'text/plain' })
+          res.end('There was an error.')
+        } else{
+          res.writeHead(200, { 'Content-Type': 'text/plain' })
+          res.end('Success. Message saved.')
+        }
+      })
+    })
+  } else {
+    res.writeHead(500, {'Content-Type': 'text/plain'});
+    res.write('Invalid response.')
+  }
+}).listen(port)
+
+console.log(`Server is listening on ${port}`)
