3878cd3945c6a7318abf65e063bf951ff1b63d68
the crafty coder <lisathuanguyen@gmail.com>
2020-05-08 21:27:23 -0700

    error

--- Lisa-Nguyen/week-2/chatroom/funFaker.js
+++
@@ -1,9 +0,0 @@
-const characters = ['Harry', 'Ron', 'Hermione', 'Draco', 'Ginny', 'Neville', 'Luna']
-const sender = characters[Math.floor(Math.random() * characters.length)];
-let receiver = characters[Math.floor(Math.random() * characters.length)];
-
-while (sender === receiver) {
-  receiver = characters[Math.floor(Math.random() * characters.length)];
-}
-
-formatData(sender, receiver, "This is a test.")
