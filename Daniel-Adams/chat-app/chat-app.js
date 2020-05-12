7aa285d2a8401925861825768737a58efba92261
Dadams4786 <58703714+Dadams4786@users.noreply.github.com>
2020-05-11 18:04:53 -0700

    Got chat app basics mostly working

---
+++ Daniel-Adams/chat-app/chat-app.js
@@ -0,0 +1,22 @@
+http.createServer(function (request, response){
+  var http = require('http')
+  var fs = require('fs')
+  const port = 8000
+  http.createServer(function (request, response){
+    if (request.method === 'Get') {
+      fs.createReadStream('./chatlog.txt').pipe(response)
+    }
+    else {
+      const readStream =
+      fs.createReadStream('./chatlog.txt');
+      const writeStream =
+        fs.createWriteStream('./chatlog.txt');
+        writeStream.on('end', () => {
+          console.log('Done');
+        });
+    
+    }
+    readStream.pipe(writeStream);
+  }).listen(+port, function () {
+    console.log('Server Listening on http://localhost:%s', port)
+  })
