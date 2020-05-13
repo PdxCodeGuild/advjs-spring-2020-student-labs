49a0f90b09ec5cf596196ebf548553a83ca4a7bd
austen cote <coteausten@gmail.com>
2020-05-12 20:48:24 -0700

    almost complete, working on filter to show completed and incomplete

--- Austen-Cote/week-3/todo/src/index.js
+++ Austen-Cote/week-3/todo/src/index.js
@@ -1,6 +1,41 @@
-const { add, getRid, completed } = require('./todo')
+const { getRid, completed } = require('./todo')
+const { filter, filterNext } = require('./filter')
 
-add()
-getRid()
-completed()
-// console.log(add(2, 3))
+const input = document.getElementById('input')
+const submit = document.getElementById('submit')
+const incompleteUl = document.getElementById('incomplete-ul')
+
+
+const items = [
+  { item: 'wash dishes', completed: true },
+  { item: 'finnish homeowrk', completed: false }
+]
+getRid(items)
+completed(items)
+add(items)
+filter(items)
+filterNext(items)
+
+function add (items) {
+  submit.addEventListener('click', function () {
+    const Todo = new Object()
+    Todo.item = input.value
+    Todo.completed = false
+    items.push(Todo)
+    // console.log(items)
+
+    const postBody = document.createElement('li')
+    postBody.innerText = Todo.item
+
+    const checkbox = document.createElement('input')
+    checkbox.type = 'checkbox'
+    checkbox.classList.add('checkbox')
+    postBody.append(checkbox)
+
+    incompleteUl.append(postBody)
+
+    document.getElementById('input').value = ''
+    console.log(items, 'add item log')
+  })
+  return items
+}
--- Austen-Cote/week-3/todo/src/todo.js
+++ Austen-Cote/week-3/todo/src/todo.js
@@ -1,60 +1,57 @@
-const input = document.getElementById('input')
-const submit = document.getElementById('submit')
 const incomplete = document.getElementById('todo-incomplete')
 const complete = document.getElementById('todo-complete')
 
-function add () {
-  submit.addEventListener('click', function () {
-    // set the todo value from the text box
-    const todo = input.value
-    // create a div and add a class to it
-    const div = document.createElement('todo-item')
-    div.classList.add('new-todo')
-    // make a li for the post and give it a 
-    const postBody = document.createElement('li')
-    postBody.innerText = todo
-    div.append(postBody)
-    // create a checkbox
-    const checkbox = document.createElement('input')
-    checkbox.type = 'checkbox'
-    checkbox.classList.add('checkbox')
-    div.append(checkbox)
-    // reset the input value
-    document.getElementById('input').value = ''
-    // add to incomplete
-    incomplete.insertBefore(div, incomplete.firstChild)
-  })
-}
-
 const deleteBtn = document.getElementById('deleteBtn')
 
-function getRid () {
+function getRid (items) {
   deleteBtn.addEventListener('click', function () {
     const checkboxGroup = incomplete.querySelectorAll("input[type='checkbox'].checkbox")
     for (let i = 0; i < checkboxGroup.length; i++) {
       if (checkboxGroup[i].checked) {
         checkboxGroup[i].parentElement.remove()
+        const completeItem = checkboxGroup[i].previousSibling.textContent
+
+        items.forEach((todo, index) => {
+          if (completeItem === todo.item) {
+            // console.log(todo, 'this is todo')
+            items.splice(index, 1)
+            // console.log(items)
+          }
+        })
       }
     }
+    console.log(items, 'remove item log')
   })
+  return items
 }
 
 const completeBtn = document.getElementById('completeBtn')
 
-function completed () {
+function completed (items) {
   completeBtn.addEventListener('click', function () {
     const checkboxGroup = incomplete.querySelectorAll("input[type='checkbox'].checkbox")
     for (let i = 0; i < checkboxGroup.length; i++) {
       if (checkboxGroup[i].checked) {
-        // console.log(checkboxGroup[i].previousSibling)
-        complete.append(checkboxGroup[i].previousSibling)
+        const completeItem = checkboxGroup[i].previousSibling.textContent
+
+        items.forEach(todo => {
+          if (completeItem === todo.item) {
+            todo.completed = true
+            // console.log(todo, 'todo')
+            // complete.append(checkboxGroup[i].previousSibling)
+          }
+        })
+        checkboxGroup[i].parentElement.remove()
+        // console.log(items)
       }
     }
+    console.log(items, 'completed log')
   })
+  return items
 }
 
 module.exports = {
-  add: add,
+  // add: add,
   getRid: getRid,
   completed: completed
 }
