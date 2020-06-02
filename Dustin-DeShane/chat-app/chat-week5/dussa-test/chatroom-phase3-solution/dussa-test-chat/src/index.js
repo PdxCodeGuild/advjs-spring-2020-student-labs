78a0ddac7777d3bac110e19eda02d19926336d9c
Dustin DeShane <Xodacidal@gmail.com>
2020-06-01 20:51:25 -0700

    Base code with ReactJS working.

--- Dustin-DeShane/chat-app/chat-week5/dussa-test/chatroom-phase3-solution/dussa-test-chat/src/index.js
+++ Dustin-DeShane/chat-app/chat-week5/dussa-test/chatroom-phase3-solution/dussa-test-chat/src/index.js
@@ -1,28 +1,59 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+const io = require('socket.io-client')
+const socket = io()
 
-const msgList = [
-  { text: 'hello! This is an example message.', date: '2020-05-15T02:05:15.596Z' },
-  { text: 'This is another message.', date: '2020-05-15T02:05:15.596Z' },
-  { text: 'test post message', date: '2020-05-15T02:05:15.596Z' },
-  { text: 'test2', date: '2020-05-15T02:05:15.596Z' }
-]
+const nickname = prompt('Enter your nickname:')
 
-function ShowMessages ({messages}) {
-  return (
-    <div>
-      <ul>
-        {messages.map((message, i) =>
-          <li key={i}>{message.text}</li>
-        )}
-      </ul>
-    </div>
-  )
+const handleSubmitMessage = function (evt) {
+  const newMessage = document.getElementById('message-text').value
+  console.log(evt)
+  evt.preventDefault()
+  const message = { text: newMessage, nick: nickname, room: 'default', date: new Date() }
+  socket.emit('chat message', message)
+  document.getElementById('send-message').reset()
+}
+
+class Chat extends React.Component {
+  constructor (props) {
+    super(props)
+    fetch('/messages')
+      .then(response => response.json())
+      .then(data => {
+        console.log('fetched data from server')
+        this.setState({ messages: data })
+      })
+    socket.on('chat message', msg => {
+      console.log('Got a message:', msg)
+      this.setState({ messages: this.state.messages.concat(msg) })
+    })
+    this.state = {
+      messages: []
+    }
+  }
+
+  render () {
+    return (
+      <div>
+        <h1>Chat House</h1>
+        <form id='send-message' onSubmit={handleSubmitMessage}>
+          <div id='chat-window'>
+            <ul>
+              {this.state.messages.map((message, i) =>
+                <li key={i}>{message.text}</li>
+              )}
+            </ul>
+          </div>
+          <input id='message-text' type='text' name='description' placeholder='message...' />
+          <button type='submit'>Send</button>
+        </form>
+      </div>
+    )
+  }
 }
 
 ReactDOM.render(
-  <ShowMessages messages={msgList} />,
+  <Chat />,
   document.getElementById('root')
 )
