5e8faa502c39a06e954a1c8e55558976ad5a170e
austen cote <coteausten@gmail.com>
2020-06-04 20:25:50 -0700

    completed login to get nickname moving to rooms - ARC

---
+++ Austen-Cote/week-6/chatroom-phase4-solution/client-react/src/App.js
@@ -0,0 +1,138 @@
+/* globals fetch prompt */
+import Chat from './chat'
+import Rooms from './rooms'
+import Home from './home'
+import React from 'react'
+import io from 'socket.io-client'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom'
+
+const socket = io()
+
+class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      messages: [],
+      nick: '',
+      room: 'general',
+      formValue: ''
+    }
+  }
+
+  componentDidMount () {
+    socket.on('chat message', msg => {
+      console.log('Got a message:', msg)
+      this.setState({ messages: this.state.messages.concat(msg) })
+    })
+
+    // Get initial list of messages
+    fetch('/messages')
+      .then(response => response.json())
+      .then(data => {
+        console.log('fetched data from server')
+        this.setState({ messages: data })
+      })
+  }
+
+  sendMessage (evt) {
+    evt.preventDefault()
+    const message = { text: this.state.formValue, nick: this.props.nick, room: this.state.room, date: new Date() }
+    socket.emit('chat message', message)
+  }
+
+  handleChangeFormInput (event) {
+    this.setState({ formValue: event.target.value })
+  }
+
+  handleAddRoom () {
+    const room = prompt('Enter a room name')
+    this.setState({ room: room })
+  }
+
+  handleChangeRoom (evt) {
+    const room = evt.target.value
+    this.setState({ room: room })
+  }
+
+  handleLogin (evt) {
+    evt.preventDefault()
+    const nick = document.getElementById('nickname').value
+    console.log(nick, 'this is the nickname')
+    this.setState({ nick: nick })
+  }
+
+  // handleChangeHomeFormInput (event) {
+  //   this.setState({ nick: event.target.value })
+  // }
+
+  getRooms () {
+    const rooms = this.state.messages.map(msg => msg.room)
+    rooms.push(this.state.room) // we have to add the currentRoom to the list, otherwise it won't be an option if there isn't already a message with that room
+    const filtered = rooms.filter(room => room) // filter out undefined or empty string
+    return Array.from(new Set(filtered)) // filters out the duplicates
+  }
+
+  render () {
+    return (
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to='/'>Home</Link>
+              </li>
+              <li>
+                <Link to='/rooms'>Room</Link>
+              </li>
+              <li>
+                <Link to='/login'>Login</Link>
+              </li>
+              <li>
+                <Link to='/logout'>Logout</Link>
+              </li>
+              <li>
+                <Link to='/signup'>Signup</Link>
+              </li>
+            </ul>
+          </nav>
+          <form id='send-message' onSubmit={this.sendMessage.bind(this)}>
+            <input id='message-text' type='text' placeholder='message...' value={this.state.formValue} onChange={this.handleChangeFormInput.bind(this)} />
+            <button type='submit'>Send</button>
+          </form>
+
+          <Switch>
+            <Route path='/rooms'>
+              <Rooms
+                nick={this.state.nick}
+                room={this.state.room}
+                rooms={this.getRooms()}
+                handleAddRoom={this.handleAddRoom.bind(this)}
+                handleChangeRoom={this.handleChangeRoom.bind(this)}
+              />
+              <Chat messages={this.state.messages} room={this.state.room} />
+            </Route>
+            <Route path='/login'>
+              {/* <Done /> */}
+            </Route>
+            <Route path='/logout'>
+              {/* <All /> */}
+            </Route>
+            <Route path='/signup'>
+              {/* <All /> */}
+            </Route>
+            <Route path='/'>
+              <Home onHandle={this.handleLogin.bind(this)} />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    )
+  }
+}
+
+export default App
---
+++ Austen-Cote/week-6/chatroom-phase4-solution/client-react/src/rooms.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Rooms (props) {
+  return <div id='rooms'>
+    <h1>Hi {props.nick}</h1>
+    <button onClick={props.handleAddRoom}>Add Room</button>
+    <label htmlFor='room-select'>Change Room:</label>
+    <select onChange={props.handleChangeRoom} name='room' id='room-select' value={props.room}>
+      <option value=''>--Select a Room--</option>
+      {props.rooms.map(room => <option key={room} value={room}>{room}</option>)}
+    </select>
+  </div>
+}
